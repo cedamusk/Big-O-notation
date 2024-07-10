5fce0bdddc67ac75a3219833f75279581c613f3b
cedamusk <130143461+cedamusk@users.noreply.github.com>
2024-07-10 09:51:53 +0300

    Add files via upload
    Insert a Node at specific position

---
+++ insert.py
@@ -0,0 +1,15 @@
+def insertAtIndex(self, data, index):
+    new_node=data
+    current_node=self.head
+    position=0
+    if position==0:
+        self.insertAtBegin(data)
+    else:
+        while(current_node != None and position+1 !=index):
+            position=position+1
+            current_node=current_node.next
+        if current_node!=None:
+            new_node.next=current_node.next
+            current_node.next=new_node
+        else:
+            print("Index is not present")
