154f25cf6864e28b801b5ad22f6b5e673be62b00
cedamusk <130143461+cedamusk@users.noreply.github.com>
2024-07-12 10:38:04 +0300

    Add files via upload
    O(n**2), a quadratic time complexity, where algorithms runtime grows quadratically with the input size

---
+++ numbers__.py
@@ -0,0 +1,7 @@
+numbers=[3,6,2,4,3,6,8,9]
+for i in range(len(numbers)):
+    for j in range(i+1, len(numbers)):
+        if numbers[i]==numbers[j]:
+            print(str(numbers)[i]+"is a duplicate")
+
+
