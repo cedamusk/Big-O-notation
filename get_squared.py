272430c8f798110680b811d0f29020018858cfdc
cedamusk <130143461+cedamusk@users.noreply.github.com>
2024-07-12 10:39:33 +0300

    Add files via upload
    O(n), linear time complexity where the algorithm grows linearly with the input size

---
+++ get_squared.py
@@ -0,0 +1,11 @@
+def get_squared_numbers(numbers):
+    squared_numbers=[]
+    for n in numbers:
+        squared_numbers.append(n*n)
+    return squared_numbers
+
+
+numbers=[2,5,8,9]
+squared_numbers=get_squared_numbers(numbers)
+print(squared_numbers)
+#O(n)
